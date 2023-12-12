4fa64dad4c8d5868adf6520065b351096eeb2b42
salmamokhtaar <salmamuqtaarsiman@gmail.com>
2023-12-13 01:42:07 +0300

    I changed h1

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer(){
     return( <div className='bg-orange-500 text-white'>
-      <h1 className='text-4xl p-4 text-center'>Built in react 2023</h1>
+      <h2 className='text-6xl p-4 text-center'>Built in react 2023</h2>
     </div>
   
     )
